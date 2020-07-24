256173f431161eaba214a8655aa7c2b52aaf1853
asd7626 <asdshq19@gmail.com>
2020-07-25 02:04:19 +0300

    class Schedule. Module PrettyTable.

--- School.py
+++ School.py
@@ -1,7 +1,6 @@
-from datetime import date
 from datetime import time
-from datetime import datetime
 from operator import itemgetter
+from prettytable import PrettyTable
 
 
 class School:
@@ -39,7 +38,6 @@ class StudentClass:
             self.students = []
         else:
             self.students = sorted(list(students))
-        self.lessons = []
 
     def add_student(self, name):
         self.students.append(name)
@@ -68,21 +66,6 @@ class StudentClass:
     def __repr__(self):
         return self.name
 
-    def all_lessons(self):
-        print('Уроки класса {}:'.format(self.name))
-        for lesson in sorted(self.lessons, key=itemgetter(1)):
-            print(lesson)
-
-    def remove_lesson(self, day, subject):
-        amount_of_lessons = len(self.lessons)
-        for lesson in self.lessons:
-            if day in lesson and subject in lesson:
-                lesson[-2].lessons.remove(lesson)
-                self.lessons.remove(lesson)
-                print('Урок удален')
-        if amount_of_lessons == len(self.lessons):
-            print('Такого урока нет в расписании')
-
 
 class Student:
 
@@ -101,11 +84,6 @@ class Teacher:
     def __init__(self, name):
         self.name = name
         self.lessons = []
-
-    def all_lessons(self):
-        print('Уроки учителя {}:'.format(self.name))
-        for lesson in sorted(self.lessons, key=itemgetter(1)):
-            print(lesson)
 
     def __str__(self):
         return self.name
@@ -135,17 +113,16 @@ class Lesson:
         self.end = end
         self.teacher = teacher
         self.subject = subject
-        self.lesson = (self.kls, datetime(year=self.day.year, month=self.day.month, day=self.day.day).strftime('%x'),
+        self.lesson = [self.kls, self.day,
                        time(hour=self.start.hour, minute=self.start.minute).strftime('%H:%M %p'),
                        time(hour=self.end.hour, minute=self.end.minute).strftime('%H:%M %p'),
-                       self.teacher, self.subject)
-
-        kls.lessons.append(self.lesson)
-        teacher.lessons.append(self.lesson)
-        print('Урок добавлен в расписание учебного класса')
+                       self.teacher, self.subject]
 
     def __iter__(self):
         return iter(self.lesson)
+
+    def __getitem__(self, item):
+        return self.lesson[item]
 
     def __str__(self):
         return '{}'.format(self.lesson)
@@ -154,9 +131,72 @@ class Lesson:
         return '{}'.format(self.lesson)
 
 
+class Schedule:
+
+    lessons = []
+
+    @classmethod
+    def add_lesson(cls, lesson):
+        cls.lessons.append(lesson)
+        print('Урок добавлен')
+
+    @classmethod
+    def __getitem__(cls, item):
+        return cls.lessons[item]
+
+    @classmethod
+    def get_lesson(cls, kls=None, day=None, teacher=None):
+        lst = [i for i in (kls, day, teacher) if i is not None]
+        answer = []
+        if len(lst) == 1:
+            answer = [lesson for lesson in cls.lessons if lst[0] in lesson]
+        elif len(lst) == 2:
+            answer = [lesson for lesson in cls.lessons if lst[0] in lesson and lst[1] in lesson]
+        elif len(lst) == 3:
+            answer = [lesson for lesson in cls.lessons if lst[0] in lesson and lst[1] in lesson and lst[2] in lesson]
+        for les in answer:
+            print(les)
+
+    @classmethod
+    def get_all_lessons(cls):
+        for lesson in sorted(cls.lessons, key=itemgetter(1)):
+            print(lesson)
+
+    @classmethod
+    def get_all_lessons_pretty(cls):
+        table = PrettyTable(['N', 'Class', 'Day', 'Start time', 'Finish time', 'Teacher', 'Subject'])
+        x = 1
+        for lesson in sorted(cls.lessons, key=itemgetter(1)):
+            table.add_row([x, lesson[0], lesson[1], lesson[2], lesson[3], lesson[4], lesson[5]])
+            x += 1
+        print(table)
+
+
 school26 = School([])
 class9B = StudentClass('9B')
+class7A = StudentClass('7A')
+class5C = StudentClass('5C')
 student1 = Student('Alex')
 teacher1 = Teacher('Mr. Johnson')
+teacher2 = Teacher('Mrs. Jane')
 math = Subject('Mathematics')
-les1 = Lesson(class9B, date(2020, 4, 28), time(8, 30), time(9, 15), teacher1, math)
+geor = Subject('Geography')
+physics = Subject('Physics')
+
+les1 = Lesson(class9B, 1, time(8, 30), time(9, 15), teacher1, math)
+les2 = Lesson(class9B, 3, time(9, 30), time(10, 15), teacher1, math)
+les3 = Lesson(class9B, 5, time(19, 30), time(10, 15), teacher1, math)
+les4 = Lesson(class7A, 1, time(10, 30), time(11, 15), teacher2, geor)
+les5 = Lesson(class7A, 2, time(9, 45), time(10, 30), teacher2, geor)
+les6 = Lesson(class7A, 4, time(12, 30), time(13, 15), teacher2, geor)
+les7 = Lesson(class5C, 3, time(15, 30), time(16, 15), teacher1, physics)
+
+Schedule.add_lesson(les1)
+Schedule.add_lesson(les2)
+Schedule.add_lesson(les3)
+Schedule.add_lesson(les4)
+Schedule.add_lesson(les5)
+Schedule.add_lesson(les6)
+Schedule.add_lesson(les7)
+
+Schedule.get_all_lessons_pretty()
