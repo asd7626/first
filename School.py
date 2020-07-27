138f55a2c4e9eded82ef4eb5785e3bca58b1418f
asd7626 <asdshq19@gmail.com>
2020-07-27 16:30:51 +0300

    Get_Lesson, New Table

--- School.py
+++ School.py
@@ -1,5 +1,5 @@
 from datetime import time
-from operator import itemgetter
+from operator import indexOf
 from prettytable import PrettyTable
 
 
@@ -114,8 +114,8 @@ class Lesson:
         self.teacher = teacher
         self.subject = subject
         self.lesson = [self.kls, self.day,
-                       time(hour=self.start.hour, minute=self.start.minute).strftime('%H:%M %p'),
-                       time(hour=self.end.hour, minute=self.end.minute).strftime('%H:%M %p'),
+                       time(hour=self.start.hour, minute=self.start.minute).strftime('%H:%M'),
+                       time(hour=self.end.hour, minute=self.end.minute).strftime('%H:%M'),
                        self.teacher, self.subject]
 
     def __iter__(self):
@@ -138,37 +138,46 @@ class Schedule:
     @classmethod
     def add_lesson(cls, lesson):
         cls.lessons.append(lesson)
-        print('Урок добавлен')
 
     @classmethod
     def __getitem__(cls, item):
         return cls.lessons[item]
 
-    @classmethod
-    def get_lesson(cls, kls=None, day=None, teacher=None):
-        lst = [i for i in (kls, day, teacher) if i is not None]
-        answer = []
-        if len(lst) == 1:
-            answer = [lesson for lesson in cls.lessons if lst[0] in lesson]
-        elif len(lst) == 2:
-            answer = [lesson for lesson in cls.lessons if lst[0] in lesson and lst[1] in lesson]
-        elif len(lst) == 3:
-            answer = [lesson for lesson in cls.lessons if lst[0] in lesson and lst[1] in lesson and lst[2] in lesson]
-        for les in answer:
-            print(les)
+    @staticmethod
+    def fnd(*args, item):
+        return all(arg in item for arg in args)
 
     @classmethod
-    def get_all_lessons(cls):
-        for lesson in sorted(cls.lessons, key=itemgetter(1)):
-            print(lesson)
+    def get_lesson(cls, *args):
+        result = []
+        for lesson in cls.lessons:
+            if cls.fnd(*args, item=lesson):
+                result.append(lesson)
+        return result
+
+    @staticmethod
+    def coordinates(lst, dt):
+        tm = ('08:30', '09:30', '10:30', '11:30', '12:30')
+        x, y = None, None
+        for i in tm:
+            if i == lst[2]:
+                y = indexOf(tm, i)
+        x = lst[1]
+        dt[y][x] += '({}, {}, {})'.format(lst[0], lst[4], lst[5])
 
     @classmethod
     def get_all_lessons_pretty(cls):
-        table = PrettyTable(['N', 'Class', 'Day', 'Start time', 'Finish time', 'Teacher', 'Subject'])
-        x = 1
-        for lesson in sorted(cls.lessons, key=itemgetter(1)):
-            table.add_row([x, lesson[0], lesson[1], lesson[2], lesson[3], lesson[4], lesson[5]])
-            x += 1
+        data = [[''] * 6 for i in range(1, len(cls.lessons))]
+        for lesson in cls.lessons:
+            cls.coordinates(lesson, data)
+        data[0][0] = '08:30 - 09:15'
+        data[1][0] = '09:30 - 10:15'
+        data[2][0] = '10:30 - 11:15'
+        data[3][0] = '11:30 - 12:15'
+        data[4][0] = '12:30 - 13:15'
+        table = PrettyTable(['', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])
+        for idx in range(len(data)):
+            table.add_row(data[idx])
         print(table)
 
 
@@ -184,12 +193,12 @@ geor = Subject('Geography')
 physics = Subject('Physics')
 
 les1 = Lesson(class9B, 1, time(8, 30), time(9, 15), teacher1, math)
-les2 = Lesson(class9B, 3, time(9, 30), time(10, 15), teacher1, math)
-les3 = Lesson(class9B, 5, time(19, 30), time(10, 15), teacher1, math)
+les2 = Lesson(class9B, 4, time(9, 30), time(10, 15), teacher1, math)
+les3 = Lesson(class9B, 5, time(9, 30), time(10, 15), teacher1, math)
 les4 = Lesson(class7A, 1, time(10, 30), time(11, 15), teacher2, geor)
-les5 = Lesson(class7A, 2, time(9, 45), time(10, 30), teacher2, geor)
+les5 = Lesson(class7A, 2, time(11, 30), time(12, 15), teacher2, geor)
 les6 = Lesson(class7A, 4, time(12, 30), time(13, 15), teacher2, geor)
-les7 = Lesson(class5C, 3, time(15, 30), time(16, 15), teacher1, physics)
+les7 = Lesson(class5C, 3, time(9, 30), time(10, 15), teacher1, physics)
 
 Schedule.add_lesson(les1)
 Schedule.add_lesson(les2)
@@ -198,5 +207,4 @@ Schedule.add_lesson(les4)
 Schedule.add_lesson(les5)
 Schedule.add_lesson(les6)
 Schedule.add_lesson(les7)
-
 Schedule.get_all_lessons_pretty()
